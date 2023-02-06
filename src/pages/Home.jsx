83a6881e223da87aa956f0d19ec4da45e0351641
Carlos <carlosng1993@gmail.com>
2023-02-06 13:54:56 +0100

    feat: Home's structure added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+import { Exercises, HeroBanner, SearchExercises } from '../components';
 
 const Home = () => {
   return (
-	<div>Home</div>
+	<Box>
+		<HeroBanner />
+		<SearchExercises />
+		<Exercises />
+	</Box>
   )
 }
 
