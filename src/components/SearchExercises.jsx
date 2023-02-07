aff1570c9f3c5281badcaadc4c83319c510ccfb9
Carlos <carlosng1993@gmail.com>
2023-02-07 11:01:54 +0100

    feat: fetchData added

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,76 +1,113 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
+import HorizontalScrollbar from './HorizontalScrollbar';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 const SearchExercises = () => {
 
-  const [ search, setSearch ] = useState('');
+	const [search, setSearch] = useState('');
+	const [exercises, setExercises] = useState([]);
+	const [bodyParts, setBodyParts] = useState([]);
 
-  const handleSearch = async () => {
-	if(search) {
-		// const exercisesData = await fetchData();
+	useEffect(() => {
+		const fetchExercisesData = async () => {
+			const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+			setBodyParts(['all', ...bodyPartsData]);
+		}
+
+		fetchExercisesData();
+	}, [] )
+
+	const handleSearch = async () => {
+		if (search) {
+			const exercisesData = await fetchData
+				('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+			const searchedExercises = exercisesData.filter(
+				(exercise) => exercise.name.toLowerCase().includes(search)
+				|| exercise.target.toLowerCase().includes(search)
+				|| exercise.equipment.toLowerCase().includes(search)
+				|| exercise.bodyPart.toLowerCase().includes(search)
+			);
+
+			setSearch('');
+			setExercises(searchedExercises);
+		}
 	}
-  }
 
-  return (
-	<Stack
-		alignItems="center"
-		mt="37px"
-		justifyContent="center"
-		p="20px"
-	>
-		<Typography
-			fontWeight={700}
-			sx={{
-				fontSize: { lg: '44px', xs: '30px' }
-			}}
-			mb="50px"
-			textAlign="center"
+	return (
+		<Stack
+			alignItems="center"
+			mt="37px"
+			justifyContent="center"
+			p="20px"
 		>
-			Awesome Exercises You <br /> Should Know
-		</Typography>
-		<Box
-			position="relative"
-			mb="72px"
-		>
-			<TextField 
+			<Typography
+				fontWeight={700}
 				sx={{
-					input: { 
-						fontWeight: '700',
-						border: 'none',
-						borderRadius: '4px'
-					},
-					width: {
-						lg: '800px',
-						xs: '350px'
-					},
-					backgroundColor: '#fff',
-					borderRadius: '40px'
+					fontSize: { lg: '44px', xs: '30px' }
 				}}
-				height="76px"
-				value={search}
-				onChange={(e) => setSearch(e.target.value.toLowerCase())}
-				placeholder="Search Exercises"
-				type="text"
-			/>
-			<Button
-				className="search-btn"
-				sx={{
-					bgcolor: '#FF2625',
-					color:'#fff',
-					textTransform: 'none',
-					width: { lg: '175px', xs: '80px'},
-					fontSize: { lg: '20px', xs: '14px'},
-					height: '56px',
-					position: 'absolute',
-					right: '0'
-				}}
-				onClick={handleSearch}
+				mb="50px"
+				textAlign="center"
 			>
-				Search
-			</Button>
-		</Box>
-	</Stack>
-  )
+				Awesome Exercises You <br /> Should Know
+			</Typography>
+			<Box
+				position="relative"
+				mb="72px"
+			>
+				<TextField
+					sx={{
+						input: {
+							fontWeight: '700',
+							border: 'none',
+							borderRadius: '4px'
+						},
+						width: {
+							lg: '800px',
+							xs: '350px'
+						},
+						backgroundColor: '#fff',
+						borderRadius: '40px'
+					}}
+					height="76px"
+					value={search}
+					onChange={(e) => setSearch(e.target.value.toLowerCase())}
+					placeholder="Search Exercises"
+					type="text"
+				/>
+				<Button
+					className="search-btn"
+					sx={{
+						bgcolor: '#FF2625',
+						color: '#fff',
+						textTransform: 'none',
+						width: { lg: '175px', xs: '80px' },
+						fontSize: { lg: '20px', xs: '14px' },
+						height: '56px',
+						position: 'absolute',
+						right: '0'
+					}}
+					onClick={handleSearch}
+				>
+					Search
+				</Button>
+			</Box>
+			<Box
+				sx={{
+					position: 'relative',
+					width: '100%',
+					p: '20px'
+				}}
+			>
+				<HorizontalScrollbar 
+					data={bodyParts}
+				/>
+			</Box>
+		</Stack>
+	)
 }
 
 export default SearchExercises;
