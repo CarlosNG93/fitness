1c537d5655b8763953824f80b97e9ef62d7c5544
Carlos <carlosng1993@gmail.com>
2023-02-07 16:09:46 +0100

    feat: Exercises & Pagination implemented

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -7,40 +7,68 @@ import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ bodyPart, exercises, setExercises }) => {
 
-  console.log(exercises);
 
-  return (
-	<Box
-		id="exercises"
-		sx={{
-			mt: { lg: '110px' }
-		}}
-		mt="50px"
-		p="20px"
-	>
-		<Typography
-			variant="h3"
-			mb="46px"
-		>
-			Showing Results
-		</Typography>
-		<Stack
-			direction="row"
+	const [currentPage, setCurrentPage] = useState(1);
+	const exercisesPerPage = 9;
+
+	const indexOfLastExercise = currentPage * exercisesPerPage;
+	const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+	const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+	const paginate = (e, value) => {
+		setCurrentPage(value);
+
+		window.scrollTo({ top: 1800, behavior: 'smooth' })
+	}
+
+	return (
+		<Box
+			id="exercises"
 			sx={{
-				gap: { lg: '110px', xs: '50px'}
+				mt: { lg: '110px' }
 			}}
-			flexWrap="wrap"
-			justifyContent="center"
+			mt="50px"
+			p="20px"
 		>
-			{exercises.map((exercise, index) => (
-				<ExerciseCard 
-					key={index}
-					exercise={exercise}
-				/>
-			))}
-		</Stack>
-	</Box>
-  )
+			<Typography
+				variant="h3"
+				mb="46px"
+			>
+				Showing Results
+			</Typography>
+			<Stack
+				direction="row"
+				sx={{
+					gap: { lg: '110px', xs: '50px' }
+				}}
+				flexWrap="wrap"
+				justifyContent="center"
+			>
+				{currentExercises.map((exercise, index) => (
+					<ExerciseCard
+						key={index}
+						exercise={exercise}
+					/>
+				))}
+			</Stack>
+			<Stack
+				mt="100px"
+				alignItems="center"
+			>
+				{exercises.length > 9 && (
+					<Pagination
+						color="standard"
+						shape="rounded"
+						defaultPage={1}
+						count={Math.ceil(exercises.length / exercisesPerPage)}
+						page={currentPage}
+						onChange={paginate}
+						size="large"
+					/>
+				)}
+			</Stack>
+		</Box>
+	)
 }
 
 export default Exercises;
